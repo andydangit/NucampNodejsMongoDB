a8d41a04f93f1bde8b88fe15e65335767773dd56
Andy Dang <andy.dang.it@gmail.com>
2020-03-18 21:06:23 -0500

    Express Generator REST API

---
+++ nucampsiteServer/routes/campsiteRouter.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const campsiteRouter = express.Router();
+
+campsiteRouter.use(bodyParser.json());
+
+campsiteRouter.route('/')
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req, res) => {
+    res.end('Will send all the campsites to you');
+})
+.post((req, res) => {
+    res.end(`Will add the campsite: ${req.body.name} with description: ${req.body.description}`);
+})
+.put((req, res) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /campsites');
+})
+.delete((req, res) => {
+    res.end('Deleting all campsites');
+});
+
+// task 1
+
+campsiteRouter.route('/:campsiteId')
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req, res) => {
+    res.end(`Will send details of campsite ${req.params.campsiteId} to you`);
+})
+.post((req, res) => {
+    res.statusCode = 403;
+    res.end(`POST operation not supported on /campsites/${req.params.campsiteId}`);
+})
+.put((req, res) => {
+    res.write(`Updating the campsite: ${req.params.campsiteId}\n`);
+    res.end(`Will update the campsite: ${req.body.name} with 
+        description: ${req.body.description}`);
+})
+.delete((req, res) => {
+    res.end(`Deleting the campsite: ${req.params.campsiteId}`)
+});
+// end of task 1
+
+
+module.exports = campsiteRouter;
+
