93e0149371bda5f552dcae6702e8dd294c44484b
Andy Dang <andy.dang.it@gmail.com>
2020-04-02 21:32:35 -0500

    Passport Facebook

--- nucampsiteServer/config.js
+++ nucampsiteServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '291309801852418',
+        clientSecret: '7f1675e8a000ee18bc2d30d9a919922f'
+    }
 }
